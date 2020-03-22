3348f9d2194b7d1a1c089e3c4c017a0ec4ad609f
Asim Poptani <asim@poptani.com>
2020-03-22 15:34:33 +0000

    Added buttons and temp image

--- HelpANeighbourApp/screens/RegisterAndLoginPageScreen.js
+++ HelpANeighbourApp/screens/RegisterAndLoginPageScreen.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
+import {  View, StyleSheet } from 'react-native'
+import Button from '../Components/Button'
+import Logo from '../Components/Logo'
+import AppStylesheet from '../AppStylesheet'
 export default class RegisterAndLoginPageScreen extends Component {
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={{justifyContent:'space-around',flex:1}}>
+                <Logo style={RegisterAndLoginPageScreenStyleSheet.logo}/>
+                <View>
+                <Button containerStyle={RegisterAndLoginPageScreenStyleSheet.button} text="Login"/>
+                 <Button containerStyle={RegisterAndLoginPageScreenStyleSheet.button} text="Register"/>
+                </View>
             </View>
         )
     }
 }
+
+const RegisterAndLoginPageScreenStyleSheet = StyleSheet.create({
+    logo:{
+        alignSelf: 'center',
+        margin:AppStylesheet.spacing,
+    },
+    button : {
+        margin: AppStylesheet.spacing
+    }
+})
