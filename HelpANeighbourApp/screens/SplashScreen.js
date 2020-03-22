dce7ada25877e603cecbc1bfaed13386eff18d30
Asim Poptani <asim@poptani.com>
2020-03-22 15:34:51 +0000

    Temp forward

--- HelpANeighbourApp/screens/SplashScreen.js
+++ HelpANeighbourApp/screens/SplashScreen.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import { Text, SafeAreaView } from 'react-native'
 import Button from '../Components/Button'
 export default class Splashscreen extends Component {
+
     render() {
+        
         return (
             <SafeAreaView>
-                <Button />
+                <Button onPress={()=>{this.props.navigation.navigate('RegisterAndLoginPageScreen')}}/>
             </SafeAreaView>
         )
     }
