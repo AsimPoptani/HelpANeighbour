5a3c375ba8c42f68830d7c2daf0053ad23988560
Asim Poptani <asim@poptani.com>
2020-03-22 20:15:24 +0000

    Added french json

--- HelpANeighbourApp/i8tn.js
+++ HelpANeighbourApp/i8tn.js
@@ -3,12 +3,13 @@ import * as RNLocalize from "react-native-localize";
 
 // All our languages
 import en from './locales/en.json';
-
+import fr from './locales/fr.json'
 // Default
 i18n.defaultLocale = 'en';
+i18n.missingBehaviour = 'guess'; // It will convert HOME_noteTitle to "HOME note title" if the value of HOME_noteTitle doesn't exist in any of the translation files.
 // Find the best one for the user
 i18n.locale = RNLocalize.findBestAvailableLanguage(['en']).languageTag;
 i18n.fallbacks = true;
-i18n.translations = { en };
+i18n.translations = { en,fr };
 
 export default i18n;
