6569a482034293a3e5c14155ff0ea4a4104211e6
Asim Poptani <asim@poptani.com>
2020-03-22 15:32:22 +0000

    Added navigations

--- HelpANeighbourApp/Navigation.js
+++ HelpANeighbourApp/Navigation.js
@@ -6,6 +6,7 @@ import LoginPageScreen from './screens/LoginPageScreen'
 import OfferingsScreen from './screens/OfferingsScreen'
 import Splashscreen from './screens/SplashScreen'
 import YourOfferingsScreen from './screens/YourOfferingsScreen'
+import RegisterAndLoginPageScreen from './screens/RegisterAndLoginPageScreen'
 // Setup navigation
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -23,6 +24,7 @@ App = () => {
             <HomeStack.Screen name="YourOfferingsScreen" component={YourOfferingsScreen}/>
             <HomeStack.Screen name="OfferingsScreen" component={OfferingsScreen}/>
             <HomeStack.Screen name="LoginPageScreen" component={LoginPageScreen}/>
+            <HomeStack.Screen name="RegisterAndLoginPageScreen" component={RegisterAndLoginPageScreen}/>
         </HomeStack.Navigator>
     )
 }
