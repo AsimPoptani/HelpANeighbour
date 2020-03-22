a63426e2740c2b23fbc27768920d93b252053103
Asim Poptani <asim@poptani.com>
2020-03-22 20:15:01 +0000

    Added bottom tabs

--- HelpANeighbourApp/Navigation.js
+++ HelpANeighbourApp/Navigation.js
@@ -7,22 +7,34 @@ import OfferingsScreen from './screens/OfferingsScreen'
 import Splashscreen from './screens/SplashScreen'
 import YourOfferingsScreen from './screens/YourOfferingsScreen'
 import RegisterAndLoginPageScreen from './screens/RegisterAndLoginPageScreen'
+import RegisterPageScreen from './screens/RegisterPageScreen'
 // Setup navigation
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // Main stack
 const HomeStack = createStackNavigator()
+// Tab navigation
+const Tab = createBottomTabNavigator();
 
+TabNavigation  = () =>{
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="YourOfferingsScreen" component={YourOfferingsScreen}/>
+            <Tab.Screen name="OfferingsScreen" component={OfferingsScreen}/>
+        </Tab.Navigator>
+    )
+}
 // All of our pages
 App = () => {
     return(
         // Disable header mode
         <HomeStack.Navigator headerMode='none'>
             <HomeStack.Screen name="Splashscreen" component={Splashscreen}/>
+            <HomeStack.Screen name="RegisterPageScreen" component={RegisterPageScreen}/>
             <HomeStack.Screen name="ForgottenPasswordScreen" component={ForgottenPasswordScreen}/>
             <HomeStack.Screen name="AddItemScreen" component={AddItemScreen}/>
-            <HomeStack.Screen name="YourOfferingsScreen" component={YourOfferingsScreen}/>
-            <HomeStack.Screen name="OfferingsScreen" component={OfferingsScreen}/>
+            <HomeStack.Screen name="Home" component={TabNavigation}/>
             <HomeStack.Screen name="LoginPageScreen" component={LoginPageScreen}/>
             <HomeStack.Screen name="RegisterAndLoginPageScreen" component={RegisterAndLoginPageScreen}/>
         </HomeStack.Navigator>
